8c22c08bf9dd4a2f78df7bd9c54801bb1f801b04
ZETA\Nik <nik.green@xtra.co.nz>
2019-10-22 15:11:45 +1300

    startingpoint

---
+++ Forever Ends/Assets/Ads.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string andorid;
+    public string Ios;
+
+    public bool testMode;
+
+    public bool playAd = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        string gameId = null;
+
+#if UNITY_ANDROID
+        gameId = andorid;
+#elif UNITY_IOS
+         gameId = Ios;
+#endif
+
+        if (Advertisement.isSupported)
+        {
+          
+            Advertisement.Initialize(gameId, testMode);
+            // Advertisement.debugMode(true);
+        }
+        Debug.Log("Unity Ads initialized: " + Advertisement.isInitialized);
+        Debug.Log("Unity Ads is supported: " + Advertisement.isSupported);
+        Debug.Log("Unity Ads test mode enabled: " + Advertisement.debugMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (playAd)
+        {
+            // Debug.Log("W");
+            if (Advertisement.IsReady("video"))
+            {
+                Debug.Log("ready");
+                Advertisement.Show("video");
+                playAd = false;
+            }
+        }
+    }
+    public void jan()
+    {
+        ShowAd();
+    }
+    public void ShowAd()
+    {
+        const string PlacementId = "video";
+
+
+        Debug.Log("here");
+
+        if (Advertisement.IsReady(PlacementId))
+        {
+
+            Debug.Log("READT");
+            Advertisement.Show(PlacementId, new ShowOptions() { resultCallback = adViewResult });
+        }
+        else
+        {
+            Debug.Log("failed");
+        }
+        Debug.Log("eND");
+    }
+    public void adViewResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log(" Player viewed complete Ad");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log(" Player Skipped Ad ");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Problem showing Ad ");
+                break;
+        }
+    }
+}
---
+++ Forever Ends/Assets/FBS.cs
@@ -0,0 +1,155 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Facebook.Unity;
+
+public class FBS : MonoBehaviour
+{
+    private string status = "Ready";
+    private string lastResponse = string.Empty;
+    public Text freindTextList;
+
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                {
+                    FB.ActivateApp();
+                }
+                else
+                {
+                    Debug.LogError("couldn't int");
+                }
+            },
+            isgameshown =>
+            {
+                if (!isgameshown)
+                {
+                    Time.timeScale = 0;
+                }
+                else
+                {
+                    Time.timeScale = 1;
+                }
+            });
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+        
+    }
+    #region login
+    public void FBLogIn()
+    {
+        var permission = new List<string>() { "Profile", "Email", "Friends" };
+        var Call = 1;
+        FB.LogInWithReadPermissions(permission, this.HandleResult);
+    }
+
+    public void FBLogOut()
+    {
+        FB.LogOut();
+    }
+
+
+    protected void HandleResult(IResult result)
+    {
+        if (result == null)
+        {
+            this.LastResponse = "Null Response\n";
+            //LogView.AddLog(this.LastResponse);
+            Debug.LogWarning("status:" + status + "response: " + lastResponse);
+            return;
+        }
+
+        //this.LastResponseTexture = null;
+
+        // Some platforms return the empty string instead of null.
+        if (!string.IsNullOrEmpty(result.Error))
+        {
+            this.Status = "Error - Check log for details";
+            this.LastResponse = "Error Response:\n" + result.Error;
+        }
+        else if (result.Cancelled)
+        {
+            this.Status = "Cancelled - Check log for details";
+            this.LastResponse = "Cancelled Response:\n" + result.RawResult;
+        }
+        else if (!string.IsNullOrEmpty(result.RawResult))
+        {
+            this.Status = "Success - Check log for details";
+            this.LastResponse = "Success Response:\n" + result.RawResult;
+        }
+        else
+        {
+            this.LastResponse = "Empty Response\n";
+        }
+
+        Debug.LogWarning("status:" + status + "response: " + lastResponse);
+       // LogView.AddLog(result.ToString());
+    }
+
+    protected string Status
+    {
+        get
+        {
+            return this.status;
+        }
+
+        set
+        {
+            this.status = value;
+        }
+    }
+
+    protected string LastResponse
+    {
+        get
+        {
+            return this.lastResponse;
+        }
+
+        set
+        {
+            this.lastResponse = value;
+        }
+    }
+    #endregion
+
+    public void FBShare()
+    {
+        FB.ShareLink(new System.Uri("http:://resocoder.com"), "cjeck it out!", "holle you went her", new System.Uri("https://www.google.com/"));
+    }
+
+    #region Invites
+    public void FBGameRequest()
+    {
+        FB.AppRequest("play this cool game", title: "Forever End!");
+    }
+    //public void FBInvite()
+    //{
+    //    FB.Mobile.AppInvite(new System.Uri("https://www.google.com/"));
+    //}
+    public void FBFreindInvite()
+    {
+        string person = "/me.friends";
+        FB.API(person, HttpMethod.GET, result =>
+          {
+
+              var dictionary = (Dictionary<string, object>)Facebook.MiniJSON.Json.Deserialize(result.RawResult);
+              List<object> freindList = (List<object>)dictionary["data"];
+              freindTextList.text = string.Empty;
+              foreach (var dict in freindList)
+              {
+                  freindTextList.text += ((Dictionary<string, object>)dict)["name"];
+              }
+          });
+       
+    }
+    #endregion
+}
